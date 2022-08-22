45b50cb7b3ae33e44b274516d9a11d7aa0589ae7
Nicolas Kerkau <nicolas.a.kerkau@gmail.com>
2022-08-22 07:29:22 +0300

    - Add project folders - Append an environment object instance to HomeView() - Create ContentModel() to parent all of the data inherited in Views

--- DinosaurEducationApp/DinosaurEducationAppApp.swift
+++ DinosaurEducationApp/DinosaurEducationAppApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct DinosaurEducationAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ DinosaurEducationApp/Models/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  DinosaurEducationApp
+//
+//  Created by Nicolas A. Kerkau on 8/22/22.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
