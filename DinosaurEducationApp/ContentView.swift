9c17608a61f73fe823e51512f85b88c2356e2b9e
Nicolas Kerkau <nicolas.a.kerkau@gmail.com>
2022-08-22 07:24:13 +0300

    Initial Commit

---
+++ DinosaurEducationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DinosaurEducationApp
+//
+//  Created by Nicolas A. Kerkau on 8/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DinosaurEducationApp/DinosaurEducationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinosaurEducationAppApp.swift
+//  DinosaurEducationApp
+//
+//  Created by Nicolas A. Kerkau on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct DinosaurEducationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
