b3d5af34c50ea52d9850985eaab255ff62846a71
Nicolas Kerkau <nicolas.a.kerkau@gmail.com>
2022-08-23 05:30:37 +0300

    - add models - add style and data resources -parse json and style data

--- DinosaurEducationApp/ViewModels/ContentModel.swift
+++ DinosaurEducationApp/ViewModels/ContentModel.swift
@@ -9,6 +9,42 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    var styleData: Data?
     
+    init () { getLocalData() }
+    
+    func getLocalData() {
+        
+        //assign a url for the json data
+        //instead of Bundle.main.path we're using .url which returns and optional url
+        let jsonURL = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            //read the file into an array of Modules
+            let jsonData = try Data(contentsOf: jsonURL!)
+            //try to decode the json into an array of modules
+            let jsonDecoder = JSONDecoder()
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            //assign parsed modules to modules property
+            self.modules = modules
+        }
+        catch {
+            //Log error
+            print("Couldn't parse local data.")
+        }
+        
+        // Pars the HTML data
+        let htmlURL = Bundle.main.url(forResource: "style", withExtension: "html")
+        do {
+            let styleData = try Data(contentsOf: styleURL)
+            self.styleData = styleData
+        }
+        catch {
+            //Catch error
+            print("Couldn't parse style data.")
+        }
+        
+    }
     
 }
